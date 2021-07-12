ecf315faf3ec86eeef044933b21d948baad0e1cd
himanshusinghltts <himanshu.singh@ltts.com>
2021-07-13 02:47:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello I am Himanshu,")
+    input()
+    print("This is just a test file for the git")
+    input()
+    print("I hope everything will go fine")
+    input()
+    print("Good Bye susan.....")
+if __name__ == "__main__":main()
